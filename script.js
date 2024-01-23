06f23111c02b85d51a7b0c74da67631ef9818c36
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2024-01-23 14:41:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 //your JS code here. If required.
-const tBody = document.getElementById("output")
+const tBody = document.getElementById("output");
+let table = document.querySelector("table");
 
 function randomTime(){
 	return Math.floor(Math.random() * 3000) + 1000;
 }
 
+function createLoading(){
+	         let row = document.createElement("tr");
+		let cell = document.createElement("td");
+		row.innerText = "loading...";
+		row.appendChild(cell);
+		table.appendChild(row);
+	return row;
+}
+
 function promise1(){
 	return new Promise((resolve,reject)=>{
 		const time = randomTime();
-            tBody.textContent = "loading...";
+            const loadingRow = createLoading();
 		  setTimeout(()=>{
+			  table.removeChild(loadingRow);
 			  resolve({promise1, time : time/1000});
 			  
         },time)
@@ -19,8 +30,10 @@ function promise1(){
 function promise2(){
 	return new Promise((resolve,reject)=>{
 		const time = randomTime();
-		  tBody.textContent = "loading...";
+	const loadingRow = createLoading();
+	
 		  setTimeout(()=>{
+			   table.removeChild(loadingRow);
 			  resolve({promise2, time : time/1000});
         },time)
 	})
@@ -29,8 +42,9 @@ function promise2(){
 function promise3(){
 	return new Promise((resolve,reject)=>{
 		const time = randomTime();
-		  tBody.textContent = "loading...";
+		const loadingRow = createLoading();
 		  setTimeout(()=>{
+			   table.removeChild(loadingRow);
 			  resolve({promise3, time : time/1000});
         },time)
 	})
@@ -39,17 +53,17 @@ function promise3(){
 
 Promise.all([promise1(),promise2(),promise3()])
     .then((results) => {
-        // const loadingRow = document.getElementById('loadingRow');
-        // loadingRow.parentNode.removeChild(loadingRow);
-
         const table = document.querySelector('table');
 
         results.forEach((result,i) => {
+		
             const row = table.insertRow(-1);
             const cell1 = row.insertCell(0);
             const cell2 = row.insertCell(1);
+		
             cell1.textContent = `promise ${i+1}`;
             cell2.textContent = result.time.toFixed(3);
+	
         });
 
         const totalRow = table.insertRow(-1);
@@ -57,5 +71,5 @@ Promise.all([promise1(),promise2(),promise3()])
         const totalCell2 = totalRow.insertCell(1);
         totalCell1.textContent = 'Total';  
         totalCell2.textContent = results.reduce((sum, result) => sum + result.time, 0).toFixed(3);
-		tBody.textContent = "";
+
     });
